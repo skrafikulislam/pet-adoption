8e081a2a3aa72e7cc6f7da172674b7a1a0e425b3
Sk Rafikul Islam <92181518+skrafikulislam@users.noreply.github.com>
2023-11-08 10:58:54 +0530

    All Initial File Have Uploaded

---
+++ script.js
@@ -0,0 +1,79 @@
+// You can use await without async while using type: "module" in the script on html file .
+
+const petPromise = await fetch(
+  "https://learnwebcode.github.io/pet-adoption-data/pets.json"
+);
+
+const pets = await petPromise.json();
+const template = document.querySelector("#animal-card");
+const wrapper = document.createElement("div");
+
+function decideAgeText(age) {
+  if (!age) {
+    return "Less Than 1 Year Old";
+  }
+
+  return age > 1 ? `${age} Years Old ` : "1 Year Old";
+}
+
+pets.forEach((pet) => {
+  const clone = template.content.cloneNode(true);
+  clone.querySelector("h3").textContent = pet.name;
+  const img = clone.querySelector("img");
+  img.src = pet.photo;
+  img.alt = `A ${pet.species} named ${pet.name}`;
+  const age = new Date().getFullYear() - pet.birthYear;
+  const ageText = decideAgeText(age);
+  clone.querySelector(".age").textContent = ageText;
+  clone.querySelector(".species").textContent = pet.species;
+  clone.querySelector(".description").textContent = pet.description;
+  clone.querySelector(".name").textContent = pet.name;
+  clone.querySelector(
+    ".primary-btn"
+  ).href = `https://learnwebcode.github.io/pet-adoption-data/pets/${pet.id}/`;
+  wrapper.appendChild(clone);
+});
+
+document.querySelector(".animals").appendChild(wrapper);
+
+const filterButton = document.querySelectorAll(".filter-nav a");
+
+filterButton.forEach((el) => {
+  el.addEventListener("click", (e) => handleFilterClick(e));
+});
+
+function handleFilterClick(e) {
+  let target = e.target;
+  //   SPAN PROBLEM FIXED HERE.
+  if (e.target.classList.contains("only-small-screen")) {
+    target = e.target.closest("a");
+  }
+  //   SPAN PROBLEM FIXED HERE.
+  e.preventDefault();
+  // MAIN WORK FOR FILTER PAGE ACOORDING TO CLICK BUTTON
+  filterButton.forEach((el) => {
+    el.classList.remove("active");
+  });
+  target.classList.add("active");
+  filterPets(target.dataset.filter);
+}
+// MAIN WORK FOR FILTER PAGE ACOORDING TO CLICK BUTTON
+
+// Another function to filter by data-filter attribute in htmlFile
+function filterPets(species) {
+  const allPets = document.querySelectorAll(".animal-card");
+  if (species == "all") {
+    allPets.forEach((el) => {
+      el.style.display = "";
+    });
+  } else {
+    allPets.forEach((el) => {
+      if (el.querySelector(".species").textContent == species) {
+        el.style.display = "";
+      } else {
+        el.style.display = "none";
+      }
+    });
+  }
+}
+// Another function to filter by data-filter attribute in htmlFile
